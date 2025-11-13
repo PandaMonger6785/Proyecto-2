0c19921e2014b4a420d88eb9fffe8643415593d9
PandaMonger6785 <jeovaniyaelhc@gmail.com>
2025-11-12 20:47:08 -0600

    Add files via upload

---
+++ Tienda/tienda_api/tienda/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import ContactMessage
+
+
+class ProductCreateForm(forms.Form):
+    name = forms.CharField(max_length=150, label="Nombre")
+    price = forms.DecimalField(max_digits=10, decimal_places=2, label="Precio")
+    category_id = forms.IntegerField(min_value=1, label="Categoría (ID)")
+    stock = forms.IntegerField(min_value=0, label="Stock", required=False)
+    description = forms.CharField(widget=forms.Textarea, required=False, label="Descripción")
+
+
+class ContactForm(forms.Form):
+    email = forms.EmailField(label="Correo")
+    comment = forms.CharField(widget=forms.Textarea, label="Comentario", max_length=2000)
+
+    def save(self):
+        return ContactMessage.objects.create(**self.cleaned_data)
